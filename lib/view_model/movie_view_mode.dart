e1ec207c4331ac2cfdaef6f0feb0ec57584c0749
Sıla Kaçmaz <sila.kacmaz@obase.com>
2022-11-20 11:43:07 +0300

    ...

---
+++ lib/view_model/movie_view_mode.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/cupertino.dart';
+import 'package:mvvm_movie_example/service/web_service.dart';
+
+import '../model/movie.dart';
+
+enum MovieState { IDLE, BUSY, ERROR }
+
+class MovieViewModel with ChangeNotifier {
+  MovieState _state = MovieState.BUSY;
+
+  List<Movie>? movieList;
+
+  MovieViewModel() {
+    movieList = [];
+    _state = MovieState.IDLE;
+    searchMovie();
+  }
+
+  MovieState get state => _state;
+
+  set state(MovieState state) {
+    _state = state;
+    notifyListeners();
+  }
+
+  Future<List<Movie>?> searchMovie() async {
+    try {
+      state = MovieState.BUSY;
+      movieList = await WebService().searchMovie();
+      state = MovieState.IDLE;
+      return movieList;
+    } catch (e) {
+      state = MovieState.ERROR;
+      return <Movie>[];
+    }
+  }
+}
