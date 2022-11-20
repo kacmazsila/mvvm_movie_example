9f01c37fd1e55e1a0b37b5043731ee7b68ad743d
Sıla Kaçmaz <sila.kacmaz@obase.com>
2022-11-20 10:46:03 +0300

    api const

---
+++ lib/constant/application_constants.dart
@@ -0,0 +1,4 @@
+class ApplicationConstant {
+  static const API_URL =
+      "https://www.omdbapi.com/?s=Batman&page=2&apikey=564727fa";
+}
--- lib/model/movie.dart
+++ lib/model/movie.dart
@@ -15,14 +15,12 @@ class Movie {
   String title;
   String year;
   String imdbId;
-  Type type;
   String poster;
 
   factory Movie.fromJson(Map<String, dynamic> json) => Movie(
         title: json["Title"],
         year: json["Year"],
         imdbId: json["imdbID"],
-        type: typeValues.map[json["Type"]],
         poster: json["Poster"],
       );
 
@@ -30,25 +28,6 @@ class Movie {
         "Title": title,
         "Year": year,
         "imdbID": imdbId,
-        "Type": typeValues.reverse[type],
         "Poster": poster,
       };
-}
-
-enum Type { MOVIE, SERIES }
-
-final typeValues = EnumValues({"movie": Type.MOVIE, "series": Type.SERIES});
-
-class EnumValues<T> {
-  Map<String, T> map;
-  Map<T, String> reverseMap;
-
-  EnumValues(this.map);
-
-  Map<T, String> get reverse {
-    if (reverseMap == null) {
-      reverseMap = map.map((k, v) => new MapEntry(v, k));
-    }
-    return reverseMap;
-  }
 }
